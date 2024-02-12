8c2ffeca6b5b7a2b1b86ca27e95a4d0719001636
ajiauliarohman <140993906+ajiauliarohman@users.noreply.github.com>
2024-02-11 18:45:55 -0800

    Add files via upload

---
+++ pertemuan_5/loop.py
@@ -0,0 +1,10 @@
+print("\nLOOPING IN PYTHON")
+print("--------------------\n")
+a = 0
+b = float(input("Masukan angka anda : "))
+while a < b: # a < b adalah syarat
+    a += 1 # Increment
+    if a == 5: # Seleksi Kondisi
+        continue # Continue Point
+    print(a)
+
