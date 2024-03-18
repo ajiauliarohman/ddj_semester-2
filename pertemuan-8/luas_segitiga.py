f7c4aba9829a42348c8409a8e651bd20c8ce3e42
ajiauliarohman <140993906+ajiauliarohman@users.noreply.github.com>
2024-03-18 09:02:42 +0700

    Add files via upload

---
+++ pertemuan-8/luas_segitiga.py
@@ -0,0 +1,5 @@
+def luas_segitiga(alas, tinggi):
+    luas = 0.5 * alas * tinggi
+    return luas
+
+print("Luas Segitiga", luas_segitiga(10, 5))
---
+++ pertemuan-8/perkenalan.py
@@ -0,0 +1,6 @@
+def perkenalan(nama, kelas, umur):
+    print("Nama saya ", nama)
+    print("Saya kelas ", kelas)
+    print("Saya berusia ", umur)
+
+perkenalan("Aji", "TKJ X-A", "16 tahun")
